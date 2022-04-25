84ea6272db5109f4e1d87a6142de2cf2e28c420e
Alan Gutierrez <alan@prettyrobots.com>
2022-04-25 10:08:41 +0000

    Fix `diff` paths.

--- foo.zsh
+++
@@ -1,9 +0,0 @@
-yq -o=json '.' < development.yaml | jq  -r '
-    [
-        .machines | to_entries[] | .key as $machine | .value | to_entries as $a | $a | keys[] |
-            $machine, ., $a[.].key,
-                ($a[.].value | to_entries as $b | ($b | length), ($b[] | .key, (
-                  .value as $v | ($v | if type == "array" then ($v | length), $v else (0, $v) end)
-                  )))
-    ] | flatten | @sh
-'
